835e874343a488b2438e5c39b6bd3a545d3d7ba3
on3dd <onedeadwave@gmail.com>
2020-11-28 05:13:29 +1000

    upd

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -1,31 +1,82 @@
 import React from 'react';
 
 import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import FixedLayout from '@vkontakte/vkui/dist/components/FixedLayout/FixedLayout';
 import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import SimpleCell from '@vkontakte/vkui/dist/components/SimpleCell/SimpleCell';
+import CardScroll from '@vkontakte/vkui/dist/components/CardScroll/CardScroll';
+import Card from '@vkontakte/vkui/dist/components/Card/Card';
+
 import Title from '@vkontakte/vkui/dist/components/Typography/Title/Title';
+import Headline from '@vkontakte/vkui/dist/components/Typography/Headline/Headline';
+import Text from '@vkontakte/vkui/dist/components/Typography/Text/Text';
+import Header from '@vkontakte/vkui/dist/components/Header/Header';
 
-import LinkButton from '../BaseUI/LinkButton';
+import { Icon28InfoOutline } from '@vkontakte/icons';
 
-const News: React.FC = () => (
-  <>
-    <Group title="Navigation Example">
-      <Div>
-        <Title level="1" weight="bold">
-          Parent route
+const Profile: React.FC = () => (
+  <Div>
+    <Group title="Info" separator="hide">
+      <Cell
+        before={
+          <Avatar
+            size={72}
+            src="https://ggscore.com/media/logo/p2888.png"
+          />
+        }
+        size="l"
+        description={
+          <Headline weight="regular">
+            Владивосток, Россия
+          </Headline>
+        }
+      >
+        <Title weight="bold" level="3">
+          Роман Кушнарев
         </Title>
-      </Div>
+      </Cell>
     </Group>
-    <Group title="Navigation Example">
-      <FixedLayout vertical="bottom">
-        <Div>
-          <LinkButton to="/profile/id">
-            Go to child route
-          </LinkButton>
-        </Div>
-      </FixedLayout>
+    <Group title="Team" separator="hide">
+      <SimpleCell
+        before={
+          <Avatar
+            size={48}
+            src="https://upload.wikimedia.org/wikipedia/ru/thumb/4/4f/Virtus.proLogo.png/1200px-Virtus.proLogo.png"
+          />
+        }
+      >
+        Virtus.Pro
+      </SimpleCell>
     </Group>
-  </>
+    <Group title="Stats" separator="hide">
+      <Cell
+        before={<Icon28InfoOutline />}
+        asideContent={<Text weight="regular">22W/8L</Text>}
+      >
+        Статистика
+      </Cell>
+    </Group>
+    <Group
+      title="Achievements"
+      header={<Header>Достижения</Header>}
+    >
+      <CardScroll>
+        <Card size="s">
+          <div style={{ width: 104, height: 96 }} />
+        </Card>
+        <Card size="s">
+          <div style={{ width: 104, height: 96 }} />
+        </Card>
+        <Card size="s">
+          <div style={{ width: 104, height: 96 }} />
+        </Card>
+        <Card size="s">
+          <div style={{ width: 104, height: 96 }} />
+        </Card>
+      </CardScroll>
+    </Group>
+  </Div>
 );
 
-export default News;
+export default Profile;
